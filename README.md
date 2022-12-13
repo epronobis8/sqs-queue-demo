7616ef381f2d7476766ea641ec89c2e3f16a09f4
Erin Pronobis <erinpronobis@Erins-MacBook-Air.local>
2022-12-13 17:19:19 -0500

    working code IaC code and updated README

--- README.md
+++ README.md
@@ -13,3 +13,31 @@ Copy the following files to the EC2 instance
 * customerInfo.json
 * queue_status.py
 * slow_producer.py
+* purge_queue.py
+* sqs_url.py
+
+Copy EC2_files.zip to EC2 instance
+* scp -i ~/Downloads/pem-file.pem ~/Documents/sqs-queue-demo/ec2_files.zip ec2-user@XX.XX.XX.XX:/home/ec2-user
+
+Install python on EC2 instance
+* sudo yum install python37 -y
+* sudo yum install python -y
+* wget https://bootstrap.pypa.io/get-pip.py
+* python3 get-pip.py
+* pip install boto3
+
+# Create SQS Queue
+python3 create_queue.py
+
+# Get SQS endpoint and update SQS_url.py file
+* aws sqs get-queue-url --queue-name mynewq
+* vi SQS_URL.py
+* update link
+
+# Demo
+* run python3 slow_producer.py
+* Check SQS in AWS Console
+* OR run python3 queue_status.py
+
+# Delete SQS queue
+* Be sure to delete manual SQS queue, since it was made with Python
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "some_custom_vpc" {
 resource "aws_subnet" "some_public_subnet" {
   vpc_id            = aws_vpc.some_custom_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "SQS-queue-demo-public-vpc"
@@ -24,7 +24,7 @@ resource "aws_subnet" "some_public_subnet" {
 resource "aws_subnet" "some_private_subnet" {
   vpc_id            = aws_vpc.some_custom_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "1a"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "SQS-queue-demo-private-vpc"
@@ -82,16 +82,68 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web_instance" {
-  ami           = "ami-0533f2ba8a1995cf9"
-  instance_type = "t3.medium"
-  key_name      = "${var.key-pair}"
+  ami                         = "ami-0533f2ba8a1995cf9"
+  instance_type               = "t3.medium"
+  key_name                    = var.key-pair
   subnet_id                   = aws_subnet.some_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.test_profile.name
   associate_public_ip_address = true
 
   tags = {
     "Name" : "SQS-queue-demo-public-ec2"
   }
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "sqs_queue_demo_ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "ec2_policy_role" {
+  name       = "ec2_sqs_policy_demo"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "ec2_policy_sqs_demo"
+  path        = "/"
+  description = "Policy to provide permission to EC2"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" = [
+      {
+        "Action" : [
+          "sqs:*"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      }
+    ]
+  })
+
 }
 
 
